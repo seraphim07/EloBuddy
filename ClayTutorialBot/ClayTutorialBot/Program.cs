e579194f9483131399038ce936bf15a41cbec077
Chaewon Lee <chris.chaewon.lee@gmail.com>
2016-10-25 21:20:53 -0700

    test

---
+++ ClayTutorialBot/ClayTutorialBot/Program.cs
@@ -0,0 +1,24 @@
+﻿using EloBuddy;
+using EloBuddy.SDK.Events;
+using System;
+
+namespace ClayTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs e)
+        {
+            Chat.OnClientSideMessage += OnClientSideMessage;
+        }
+
+        private static void OnClientSideMessage(ChatClientSideMessageEventArgs args)
+        {
+            Chat.Print(args.Message);
+        }
+    }
+}
